b9dad984aa2ebc59a3857c3e7d724536ae47677a
Wadee01 <ralf.hernandez150318@my.lsgh.edu.ph>
2021-04-29 12:57:55 +0800

    3 files modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -18,8 +18,10 @@ cicMessage: string;
   constructor(public navCtrl: NavController) {
 
    }
-calculateCIC(){
+calculateBMR(){
 this.BMR = (10 * this.weight + 6.25 * this.height - 5 * this.age + this.male - this.female);
+this.BMR = parseFloat(this.BMR.toFixed(2));
+
 if (this.gender = "m") {
  this.male = 5
  this.female = 0
